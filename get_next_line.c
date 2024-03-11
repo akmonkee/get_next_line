0185172ce57889975e05745856e2af9eac43d6b2
akmonkee <124872194+akmonkee@users.noreply.github.com>
2024-03-11 15:19:25 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msisto <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 14:15:23 by msisto            #+#    #+#             */
+/*   Updated: 2024/03/11 14:15:30 by msisto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*gnl_strjoin(char *line, char *buf, int len)
+{
+	int		i;
+	int		c;
+	char	*fstr;
+
+	if (!line)
+	{
+		line = (char *)malloc(sizeof(char) * 1);
+		if(!line)
+			return (NULL);
+		line[0] = '\0';
+	}
+	if (!buf)
+		return (NULL);
+	fstr = (char *)malloc(ft_strlen(line) + len);
+	if (!fstr)
+		return (NULL);
+	i = 0;
+	c = 0;
+	while (line[i])
+		fstr[c++] = line[i++];
+	i = 0;
+	while (buf[i])
+		fstr[c++] = buf[i++];
+	fstr[c] = '\0';
+	free(line);
+	return (fstr);
+}
+void	str_clear(char *buf, char *storage)
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	k = 0;
+	while (buf[i] != '\n')
+		i++;
+	buf[i] = '\0';
+	while (buf[++i] != '\0')
+		k++;
+	storage = malloc( k + 1);
+	storage[k] = '\0';
+	i = i - k;
+	k = -1;
+	while (buf[i] != '\0')
+	{
+		storage[++k] = buf[i];
+		i++;
+	}
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str != (char)c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return ((char *) str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char		*storage;
+	char			buf[BUFFER_SIZE + 1];
+	char			*ret;
+	int				len;
+
+	buf[BUFFER_SIZE] = '\0';
+	while(len = read(fd, buf, BUFFER_SIZE) > 0)
+	{
+		if (ft_strchr(buf, '\n') == NULL)
+			gnl_strjoin(ret, buf, len);
+		else
+		{
+			ret = gnl_strjoin(ret, buf, ft_strchr(buf, '\n') - buf);
+			str_clear(buf, storage); //funzione che mette quello che sta dopo \n all-innizio e il resto a \0
+			break ;
+		}
+	}
+	if (ret && *ret)
+		return (ret);
+	free(ret);
+	return (NULL);
+}
+
+int main()
+{
+	int		fd;
+	char	*output;
+	fd = open("./file", O_RDONLY);
+	output = get_next_line(fd);
+	while (output != NULL)
+	{
+		printf("%s", output);
+		free(output);
+		output = get_next_line(fd);
+	}
+	free(output);
+	close(fd);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msisto <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 15:07:53 by msisto            #+#    #+#             */
+/*   Updated: 2024/03/11 15:07:59 by msisto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*gnl_strjoin(char *line, char *buf, int len);
+void	str_clear(char *buf, char *storage);
+char	*ft_strchr(const char *str, int c);
+char	*get_next_line(int fd);
+int		ft_strlen(const char *str);
+
+#endif
